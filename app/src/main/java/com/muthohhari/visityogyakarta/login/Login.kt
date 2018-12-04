a3a471185fd3dcddcbe858cce701cf8efd074c9d
aliToha <34850445+aliToha@users.noreply.github.com>
2018-12-04 23:56:32 +0700

    fix error multi dex, delete variable

--- app/src/main/java/com/muthohhari/visityogyakarta/login/Login.kt
+++ app/src/main/java/com/muthohhari/visityogyakarta/login/Login.kt
@@ -1,11 +1,11 @@
 package com.muthohhari.visityogyakarta.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -14,8 +14,8 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.muthohhari.visityogyakarta.R
-import com.muthohhari.visityogyakarta.signup.SignUp
 import com.muthohhari.visityogyakarta.main.Main
+import com.muthohhari.visityogyakarta.signup.SignUp
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.mtrl_layout_snackbar_include.*
 import org.jetbrains.anko.AnkoLogger
@@ -23,9 +23,9 @@ import org.jetbrains.anko.longToast
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
-class Login : AppCompatActivity(),View.OnClickListener, AnkoLogger {
+class Login : AppCompatActivity(), View.OnClickListener, AnkoLogger {
     private var mAuth: FirebaseAuth? = null
-    private lateinit var googleSignInClient : GoogleSignInClient
+    private lateinit var googleSignInClient: GoogleSignInClient
     private val RC_SIGN_IN = 101
     private val message = "snackbar gagal"
 
@@ -47,44 +47,43 @@ class Login : AppCompatActivity(),View.OnClickListener, AnkoLogger {
 
     override fun onClick(view: View) {
         val id = view.id
-        when(id){
+        when (id) {
             R.id.createAccountText -> startActivity<SignUp>()
-            R.id.loginPasswordButton -> signIn(editTextEmail.text.toString(),editTextPassword.text.toString())
+            R.id.loginPasswordButton -> signIn(editTextEmail.text.toString(), editTextPassword.text.toString())
             R.id.loginGoogleButton -> googleSignIn()
         }
 
     }
 
     private fun googleSignIn() {
-            val signInIntent = googleSignInClient.signInIntent
-            startActivityForResult(signInIntent, RC_SIGN_IN)
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     private fun signIn(email: String, password: String) {
         if (!validateForm(email, password)) {
             return
         }
-        mAuth!!.signInWithEmailAndPassword(email,password).addOnCompleteListener(this){ it ->
-            if (it.isSuccessful){
-                val user = mAuth!!.currentUser
+        mAuth!!.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { it ->
+            if (it.isSuccessful) {
                 startActivity<Main>()
                 finish()
-            }else{
+            } else {
                 longToast("Authentication Failed")
             }
         }
     }
 
-    private fun validateForm(email: String, password: String):Boolean {
-        if(TextUtils.isEmpty(email)){
+    private fun validateForm(email: String, password: String): Boolean {
+        if (TextUtils.isEmpty(email)) {
             toast("Enter Email Address")
             return false
         }
-        if (TextUtils.isEmpty(password)){
+        if (TextUtils.isEmpty(password)) {
             toast("Enter Password")
             return false
         }
-        if (password.length < 6){
+        if (password.length < 6) {
             toast("Password to short, enter minimum 6 characters")
             return false
         }
@@ -94,35 +93,34 @@ class Login : AppCompatActivity(),View.OnClickListener, AnkoLogger {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-            if (requestCode == RC_SIGN_IN) {
-                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-                try {
-                    val account = task.getResult(ApiException::class.java)
-                    firebaseAuthWithGoogle(account!!)
-                } catch (e: ApiException) {
-                    Log.w("TAG", "Google sign in failed", e)
-                }
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                Log.w("TAG", "Google sign in failed", e)
             }
         }
+    }
 
     private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
-            Log.d("TAG", "firebaseAuthWithGoogle:" + account.id!!)
+        Log.d("TAG", "firebaseAuthWithGoogle:" + account.id!!)
 
-            val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-            mAuth!!.signInWithCredential(credential)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        Log.d("TAG", "signInWithCredential:success")
-                        val user = mAuth!!.currentUser
-                        startActivity<Main>()
-                        finish()
-                    } else {
-                        Log.w("TAG", "signInWithCredential:failure", task.exception)
-                        snackbar_text.text = message
-                    }
-
-                    // ...
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        mAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("TAG", "signInWithCredential:success")
+                    startActivity<Main>()
+                    finish()
+                } else {
+                    Log.w("TAG", "signInWithCredential:failure", task.exception)
+                    snackbar_text.text = message
                 }
+
+                // ...
+            }
 
     }
 }
